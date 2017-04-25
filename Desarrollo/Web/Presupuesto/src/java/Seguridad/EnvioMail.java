a339c1413e47c2672648ca31e0caee9af2ae79b9
rodriguezja <rodriguezmja@gmail.com>
2017-04-25 04:43:33 -0400

    Creada la clase EnvioMail, esta clase envía un correo a cualquier destinatario.

---
+++ Desarrollo/Web/Presupuesto/src/java/Seguridad/EnvioMail.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Seguridad;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+//import javax.swing.JOptionPane;
+/**
+ *
+ * @author rodriguezja
+ */
+public class EnvioMail {
+    private static String Username = "";
+    private static String PassWord = "";
+    private String Mensage = "";
+    private String To = "";
+    private String Subject = "";
+
+    public EnvioMail(String correoTo) {
+        this.setTo(correoTo);
+    }
+    
+    public void SendMail() {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "25");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(Username, PassWord);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(To));
+            message.setSubject(Subject);
+            message.setText(Mensage);
+
+            Transport.send(message);
+            System.out.println("su mensaje ha sido enviado");
+            //JOptionPane.showMessageDialog(this, "Su mensaje ha sido enviado");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static String getUsername() {
+        return Username;
+    }
+
+    public static void setUsername(String Username) {
+        EnvioMail.Username = Username;
+    }
+
+    public static String getPassWord() {
+        return PassWord;
+    }
+
+    public static void setPassWord(String PassWord) {
+        EnvioMail.PassWord = PassWord;
+    }
+
+    public String getMensage() {
+        return Mensage;
+    }
+
+    public void setMensage(String Mensage) {
+        this.Mensage = Mensage;
+    }
+
+    public String getTo() {
+        return To;
+    }
+
+    public void setTo(String To) {
+        this.To = To;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String Subject) {
+        this.Subject = Subject;
+    }
+    
+}
