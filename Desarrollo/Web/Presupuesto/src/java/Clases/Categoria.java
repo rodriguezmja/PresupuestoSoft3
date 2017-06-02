2f9c122cff4848ec510e77c28ede6e2e339fd4f8
rodriguezja <rodriguezmja@gmail.com>
2017-06-01 23:28:31 -0400

    Modificacion de Clases del Modelo, creacion de procedimientos

--- Desarrollo/Web/Presupuesto/src/java/Clases/Categoria.java
+++ Desarrollo/Web/Presupuesto/src/java/Clases/Categoria.java
@@ -5,6 +5,11 @@
  */
 package Clases;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rodriguezja
@@ -13,9 +18,83 @@ public class Categoria {
     private int categoria_id;
     private String nombre;
     private String descripcion;
-    private Usuario usuario;
+    private int usuario_id;
+    
+    private Conn con;
 
     public Categoria() {
+    }
+
+    public Categoria(int categoria_id, String nombre, String descripcion, int usuario_id) {
+        this.categoria_id = categoria_id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.usuario_id = usuario_id;
+    }
+    
+     
+
+    public Categoria(Conn con) {
+        this.con = con;
+    }
+    
+    
+    
+     public void setCon(Conn con) {
+        this.con = con;
+    }
+
+    public List<Categoria> cargar(ResultSet rs) {
+        List<Categoria> lista = new ArrayList();
+        try {
+            while (rs.next()) {
+                Categoria aux = new Categoria(con);
+                aux.setCategoria_id(rs.getInt("categoria_id"));
+                aux.setNombre(rs.getString("nombre"));
+                aux.setDescripcion(rs.getString("descripcion"));
+                aux.setUsuario(rs.getInt("usuario_id"));
+               
+                lista.add(aux);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al cargar los datos");
+        }
+        return lista;
+    }
+
+    public List<Categoria> todo() throws SQLException {
+        String consulta = "select * from Categoria";
+        return cargar(con.consultar(consulta));
+    }
+
+    public Categoria buscarXid(int id) throws SQLException {
+        String consulta = "select * from Categoria where categoria_id=" + id;
+        ResultSet rs = con.consultar(consulta);
+        List<Categoria> lis = cargar(rs);
+        if (lis.size() > 0) {
+            return lis.get(0);
+        }
+        return null;
+    }
+
+    public Categoria buscarxNombre(String nombreCategoria) throws SQLException {
+        String consulta = "select * from Categoria where nombre='"+ nombreCategoria +"'"; 
+        ResultSet rs = con.consultar(consulta);
+        List<Categoria> lis = cargar(rs);
+        if (lis.size() > 0) {
+            return lis.get(0);
+        }
+        return null;
+    }
+
+    public void modificar() throws SQLException {
+        String consulta = "update Categoria set categoria_id = " + categoria_id + ", nombre = '" + nombre + "', descripcion = '" + descripcion + "', usuario_id = '" + usuario_id + "' where categoria_id=" + categoria_id;
+        con.manipular(consulta);
+    }
+
+    public void insertar() throws SQLException {            
+        String consulta = "insert into  presupuesto.dbo.Categoria(nombre, descripcion, usuario_id) values('"+ nombre + "','" + descripcion + "','" + usuario_id + "')";
+        con.manipular(consulta);     
     }
 
     public int getCategoria_id() {
@@ -42,12 +121,12 @@ public class Categoria {
         this.descripcion = descripcion;
     }
 
-    public Usuario getUsuario() {
-        return usuario;
+    public int getUsuario() {
+        return usuario_id;
     }
 
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
+    public void setUsuario(int usuario_id) {
+        this.usuario_id = usuario_id;
     }
     
     
--- Desarrollo/Web/Presupuesto/src/java/Clases/SubCategoria.java
+++ Desarrollo/Web/Presupuesto/src/java/Clases/SubCategoria.java
@@ -5,6 +5,11 @@
  */
 package Clases;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rodriguezja
@@ -13,10 +18,95 @@ public class SubCategoria {
     private int subCategoria_id;
     private String nombre;
     private String descripcion;
-    private Categoria categoria;
+    private int categoria_id;
+    
+    private Conn con;
 
     public SubCategoria() {
     }
+
+    public SubCategoria(Conn con) {
+        this.con = con;
+    }
+    
+    
+
+    public SubCategoria(int subCategoria_id, String nombre, String descripcion, int categoria_id) {
+        this.subCategoria_id = subCategoria_id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.categoria_id = categoria_id;
+    }
+    
+    
+    public List<SubCategoria> cargar(ResultSet rs) {
+        List<SubCategoria> lista = new ArrayList();
+        try {
+            while (rs.next()) {
+                SubCategoria aux = new SubCategoria(con);
+                aux.setSubCategoria_id(rs.getInt("subCategoria_id"));
+                aux.setNombre(rs.getString("nombre"));
+                aux.setDescripcion(rs.getString("descripcion"));
+                aux.setCategoria_id(rs.getInt("categoria_id"));
+               
+                lista.add(aux);
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error al cargar los datos");
+        }
+        return lista;
+    }
+
+    public List<SubCategoria> todo() throws SQLException {
+        String consulta = "select * from SubCategoria";
+        return cargar(con.consultar(consulta));
+    }
+
+    public SubCategoria buscarXid(int id) throws SQLException {
+        String consulta = "select * from SubCategoria where subcategoria_id=" + id;
+        ResultSet rs = con.consultar(consulta);
+        List<SubCategoria> lis = cargar(rs);
+        if (lis.size() > 0) {
+            return lis.get(0);
+        }
+        return null;
+    }
+
+    public SubCategoria buscarxNombre(String nombreSubCategoria) throws SQLException {
+        String consulta = "select * from SubCategoria where nombre='"+ nombreSubCategoria +"'"; 
+        ResultSet rs = con.consultar(consulta);
+        List<SubCategoria> lis = cargar(rs);
+        if (lis.size() > 0) {
+            return lis.get(0);
+        }
+        return null;
+    }
+
+    public void modificar() throws SQLException {
+        String consulta = "update SubCategoria set subcategoria_id = " + subCategoria_id + ", nombre = '" + nombre + "', descripcion = '" + descripcion + "', categoria_id = '" + categoria_id + "' where subcategoria_id=" + subCategoria_id;
+        con.manipular(consulta);
+    }
+
+    public void insertar() throws SQLException {            
+        String consulta = "insert into  presupuesto.dbo.SubCategoria(nombre, descripcion, categoria_id) values('"+ nombre + "','" + descripcion + "','" + categoria_id + "')";
+        con.manipular(consulta);     
+    }
+
+    public int getCategoria_id() {
+        return categoria_id;
+    }
+
+    public void setCategoria_id(int categoria_id) {
+        this.categoria_id = categoria_id;
+    }
+
+    
+
+    public void setCon(Conn con) {
+        this.con = con;
+    }
+    
+    
 
     public int getSubCategoria_id() {
         return subCategoria_id;
@@ -40,15 +130,6 @@ public class SubCategoria {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
-    
+    } 
     
 }
