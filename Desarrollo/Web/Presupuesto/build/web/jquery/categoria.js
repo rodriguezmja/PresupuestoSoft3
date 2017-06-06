137adae2dbfaabd2c696949f2ff482c5140e3ee9
rodriguezja <rodriguezmja@gmail.com>
2017-06-06 18:26:05 -0400

    a

--- Desarrollo/Web/Presupuesto/build/web/jquery/categoria.js
+++ Desarrollo/Web/Presupuesto/build/web/jquery/categoria.js
@@ -19,20 +19,41 @@ function  crearCategoria() {
 
 
 function  obtenerCategoria() {
-    $.get("api/controladorcategoria/obtenercategoria", function (response) {
+    var UsuarioId = localStorage.getItem("Usuario").split(",")[0];
+    $.get("api/controladorcategoria/obtenercategoria",  {      
+        usuario_id: UsuarioId}, function (response) {
         listaCategoria = $.parseJSON(response.message);
-        var html = "<td>1</td>";
-        var html = "<td>Universidad</td>";
-        var html = "<td>Gastos de la universidad</td>";
+        var html ="<tr>";
+        var  html = "<td class='1'></td>";
+        var  html = "<td class='2'></td>";
+        var  html = "<td class='3'></td>";
+        var  html = "<td class='td-actions text-right'>";
+        var  html = "<button title='' class='btn btn-primary btn-simple btn-xs' type='button' data-original-title='Edit Task' rel='tooltip'>";
+        var  html = "<i class='material-icons'>edit</i>";
+        var  html = "<div class='ripple-container'></div></button>";
+        var  html = "<button title='' class='btn btn-danger btn-simple btn-xs' type='button' data-original-title='Remove' rel='tooltip'>";
+        var  html = "<i class='material-icons'>close</i>";
+        var  html = "</button>";
+        var  html = "</td>";
+        var  html ="</tr>";
         
 
         for (var i = 0; i < listaCategoria.length; i++) {
+            html +="<tr>";
+            html += "<td id='1'>" + listaCategoria[i].id + "</td>";
+            html += "<td id='2'>" + listaCategoria[i].nombre + "</td>";
+            html += "<td id='3'>" + listaCategoria[i].descripcion + "</td>";
+            html += "<td class='td-actions text-right'>";
+            html += "<button title='' class='btn btn-primary btn-simple btn-xs' type='button' data-original-title='Edit Task' rel='tooltip'>";
+            html += "<i class='material-icons'>edit</i>";
+            html += "<div class='ripple-container'></div></button>";
+            html += "<button title='' class='btn btn-danger btn-simple btn-xs' type='button' data-original-title='Remove' rel='tooltip'>";
+            html += "<i class='material-icons'>close</i>";
+            html += "</button>";
+            html += "</td>";
             
-            html = "<td>" + listaCategoria[i].categoria_id + "</td>";
-            html = "<td>" + listaCategoria[i].nombre + "</td>";
-            html = "<td>" + listaCategoria[i].descripcion + "</td>";
-            
+            html +="</tr>";
         }
-        $("#filacategoria").html(html);
+        $("#filacat").html(html);
     });
 }
