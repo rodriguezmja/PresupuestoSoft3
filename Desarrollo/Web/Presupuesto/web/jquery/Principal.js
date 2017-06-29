1c25d02302d76d4900f6a12cfc5f7b05273b6ffd
rodriguezja <rodriguezmja@gmail.com>
2017-06-29 19:28:18 -0400

    Cerrar sesion de usuario

--- Desarrollo/Web/Presupuesto/web/jquery/Principal.js
+++ Desarrollo/Web/Presupuesto/web/jquery/Principal.js
@@ -2,4 +2,9 @@ var usuarioID = 0;
 $(document).ready(function () {
     if(localStorage.getItem("Usuario")===null){
     window.location.href = "Login.html";}
+   $("#usuariologueado").text(localStorage.getItem("Usuario").split(",")[1]);
 });
+
+function salirsesion(){
+    localStorage.clear();
+}
